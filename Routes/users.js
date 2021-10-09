ebf47214e34688c1e5e652611a77ef20d84feefb
Pravesh Vyas <praveshvyas.18@gmail.com>
2021-10-09 14:13:52 +0530

    friend Route

--- Routes/users.js
+++ Routes/users.js
@@ -57,7 +57,32 @@ UserRouter.get('/:id', async (req, res) => {
   }
 });
 
-// get all users
+// get friends
+UserRouter.get("/friends/:userId", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.userId);
+    const friends = await Promise.all(
+      user.followings.map((friendId) => {
+        return User.findById(friendId);
+      })
+    );
+    let friendList = [];
+    friends.map((friend) => {
+      const { _id, username, profilePicture } = friend;
+      friendList.push({ _id, username, profilePicture });
+    });
+    res.status(200).json(friendList)
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+
+
+
+
+
 //follow a user
 UserRouter.put('/:id/follow', async (req, res) => {
   if (req.body.userId !== req.params.id) {
