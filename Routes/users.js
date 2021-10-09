503f2f36c9fe800aab3e4719da103cf8d0e7f92f
Pravesh Vyas <praveshvyas.18@gmail.com>
2021-10-09 14:54:08 +0530

    base user

--- Routes/users.js
+++ Routes/users.js
@@ -120,4 +120,21 @@ UserRouter.put('/:id/unfollow', async (req, res) => {
   }
 });
 
+//single user
+UserRouter.get("/", async (req, res) => {
+  const userId = req.query.userId;
+  const username = req.query.username;
+  try {
+    const user = userId
+      ? await User.findById(userId)
+      : await User.findOne({ username: username });
+    const { password, updatedAt, ...other } = user._doc;
+    res.status(200).json(other);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+
 export default UserRouter;
